cde4a07c6b6fa454f1410105ed9a32ac5c45dd66
prabin <bhusal1008@gmail.com>
2026-06-29 15:07:08 +0545

    todo app

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,19 @@ const App = () => {
   return (
     <main>
       <div className="relative">
-        <img
-          src="./images/bg.png"
-          alt=""
-          className="`bg-[rgba(0,0,0,0.4)] min-w-full h-screen"
-        />
+        <img src="./images/bg.png" alt="" className="` min-w-full h-screen" />
       </div>
-      <div className="absolute left-[30%] top-[50%]  flex flex-col">
-        <h3>Task Management & To-Do List</h3>
-        <p>
-          This Productive tool is developed to help you better manage your task
-          Project-wise Conveniently
-        </p>
-        <button className="bg-blue-600 text-white font-bold">Let's Start</button>
+      <div className="absolute left-0 top-0 flex flex-col justify-end  items-center  text-white min-h-screen min-w-screen pb-20  bg-[rgba(0,0,0,0.6)]">
+        <div className="px-10 space-y-4 text-center">
+          <h3 className="font-bold text-4xl text-jus">Task Management & To-Do List</h3>
+          <p>
+            This Productive tool is developed to help you better manage your
+            task Project-wise Conveniently
+          </p>
+          <button className="bg-blue-600 text-white text-xl font-bold px-6 py-2 rounded-2xl">
+            Let's Start
+          </button>
+        </div>
       </div>
     </main>
   );
